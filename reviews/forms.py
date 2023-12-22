43ab63268ab5b9cb20713768bc02a5147abd2ca3
yusuf <yusuftechie+git@gmail.com>
2023-12-22 11:36:03 +0530

    added static files

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,5 +2,12 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
-    
+    user_name = forms.CharField(
+        label='Your Name', 
+        max_length=50,
+        required=True,
+        error_messages={
+            'required': 'Your name must not be empty!',
+            'max_length': "Please enter a shorter name!"
+        }
+        )
